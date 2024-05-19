1bbb2285780c02b3cbd265c430f20f9b3bf74f2e
Neil Hyde <neil.hyde17@gmail.com>
2024-05-19 00:53:17 -0500

    initial commit, foundation in place

---
+++ impossible_button.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+from random import randint
+
+root = tk.Tk()
+root.title("Impossible Button")
+
+def add_point():
+    global points
+    global value
+    global chance
+    global highScore
+
+    points += 1
+    press.config(text=str(points))
+
+    chance = randint(1, 100)
+
+    value -= 1
+    if chance > value:
+        press.config(text="You Lose "+ str(points), bg="red", state="disabled")
+    print(value)
+    print(chance)
+
+def high_score():
+    global highScore
+
+    if points >= highScore:
+        highScore = points
+        label.config(text="High Score: " + str(highScore))
+
+def start_over():
+    global points
+    global value
+
+    points = 0
+    value = 100
+    press.config(text="Press", bg="light gray", state="normal")
+
+
+
+
+points = 0
+value = 100
+chance = randint(1, 100)
+highScore = 0
+
+
+press = tk.Button(root, text="Press", width=20, bg="light gray", command=lambda:[add_point(), high_score()])
+press.pack()
+press.place(x=120, y=100)
+
+reset = tk.Button(root, text="Reset", width=10, bg="light gray", command=lambda:start_over())
+reset.pack()
+reset.place(x=300, y=20)
+
+label = tk.Label(root, text="High Score: " + str(points))
+label.pack()
+label.place(x=10, y=20)
+
+root.geometry("400x200")
+root.mainloop()
